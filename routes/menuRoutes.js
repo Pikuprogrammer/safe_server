31ef92bb78f4c06d632abde536ad3a8731b067af
Pinku Kumar <anandpiku552@gmail.com>
2026-07-08 16:26:29 +0530

    route

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -10,11 +10,13 @@ const {
 } = require('../controllers/menuController');
 const protect = require('../middleware/auth');
 const upload = require('../middleware/upload');
+const upload = require("../config/multer");
 
 router.get('/', getMenuItems);
 router.get('/all', protect, getAllMenuItems);
 router.get('/:id', getMenuItem);
 router.post('/', protect, upload.single('image'), createMenuItem);
+// router.post("/",upload.single("image"), createMenuItem);
 router.put('/:id', protect, upload.single('image'), updateMenuItem);
 router.delete('/:id', protect, deleteMenuItem);
 
